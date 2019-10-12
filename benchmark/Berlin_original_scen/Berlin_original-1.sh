9f92f0a5c606cbe21714787f384e162b211e4c69
husl903 <hushuli12@gmail.com>
2019-10-11 21:08:18 -0600

    upload the benchmark

---
+++ benchmark/Berlin_original_scen/Berlin_original-1.sh
@@ -0,0 +1,50 @@
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-10.scen_1000_agents.yaml -o Berlin_1_256-even-10.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-11.scen_1000_agents.yaml -o Berlin_1_256-even-11.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-12.scen_1000_agents.yaml -o Berlin_1_256-even-12.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-13.scen_1000_agents.yaml -o Berlin_1_256-even-13.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-14.scen_1000_agents.yaml -o Berlin_1_256-even-14.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-15.scen_1000_agents.yaml -o Berlin_1_256-even-15.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-16.scen_1000_agents.yaml -o Berlin_1_256-even-16.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-17.scen_1000_agents.yaml -o Berlin_1_256-even-17.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-18.scen_1000_agents.yaml -o Berlin_1_256-even-18.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-19.scen_1000_agents.yaml -o Berlin_1_256-even-19.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-1.scen_1000_agents.yaml -o Berlin_1_256-even-1.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-20.scen_1000_agents.yaml -o Berlin_1_256-even-20.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-21.scen_1000_agents.yaml -o Berlin_1_256-even-21.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-22.scen_1000_agents.yaml -o Berlin_1_256-even-22.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-23.scen_1000_agents.yaml -o Berlin_1_256-even-23.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-24.scen_1000_agents.yaml -o Berlin_1_256-even-24.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-25.scen_1000_agents.yaml -o Berlin_1_256-even-25.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-2.scen_1000_agents.yaml -o Berlin_1_256-even-2.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-3.scen_1000_agents.yaml -o Berlin_1_256-even-3.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-4.scen_1000_agents.yaml -o Berlin_1_256-even-4.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-5.scen_1000_agents.yaml -o Berlin_1_256-even-5.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-6.scen_1000_agents.yaml -o Berlin_1_256-even-6.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-7.scen_1000_agents.yaml -o Berlin_1_256-even-7.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-8.scen_1000_agents.yaml -o Berlin_1_256-even-8.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-9.scen_1000_agents.yaml -o Berlin_1_256-even-9.scen_1000_agents.yaml.res
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-10.scen_1000_agents.yaml -o Berlin_1_256-even-10.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-11.scen_1000_agents.yaml -o Berlin_1_256-even-11.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-12.scen_1000_agents.yaml -o Berlin_1_256-even-12.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-13.scen_1000_agents.yaml -o Berlin_1_256-even-13.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-14.scen_1000_agents.yaml -o Berlin_1_256-even-14.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-15.scen_1000_agents.yaml -o Berlin_1_256-even-15.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-16.scen_1000_agents.yaml -o Berlin_1_256-even-16.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-17.scen_1000_agents.yaml -o Berlin_1_256-even-17.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-18.scen_1000_agents.yaml -o Berlin_1_256-even-18.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-19.scen_1000_agents.yaml -o Berlin_1_256-even-19.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-1.scen_1000_agents.yaml -o Berlin_1_256-even-1.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-20.scen_1000_agents.yaml -o Berlin_1_256-even-20.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-21.scen_1000_agents.yaml -o Berlin_1_256-even-21.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-22.scen_1000_agents.yaml -o Berlin_1_256-even-22.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-23.scen_1000_agents.yaml -o Berlin_1_256-even-23.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-24.scen_1000_agents.yaml -o Berlin_1_256-even-24.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-25.scen_1000_agents.yaml -o Berlin_1_256-even-25.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-2.scen_1000_agents.yaml -o Berlin_1_256-even-2.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-3.scen_1000_agents.yaml -o Berlin_1_256-even-3.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-4.scen_1000_agents.yaml -o Berlin_1_256-even-4.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-5.scen_1000_agents.yaml -o Berlin_1_256-even-5.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-6.scen_1000_agents.yaml -o Berlin_1_256-even-6.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-7.scen_1000_agents.yaml -o Berlin_1_256-even-7.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-8.scen_1000_agents.yaml -o Berlin_1_256-even-8.scen_1000_agents.yaml.res >>Berlin.out.txt
+./JPSSIPP -i ../benchmark/Berlin_original_scen/Berlin_1_256-even-9.scen_1000_agents.yaml -o Berlin_1_256-even-9.scen_1000_agents.yaml.res >>Berlin.out.txt
